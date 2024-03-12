b27da25d7e97f7284ec9942cfb888759f38b32fa
Michael Barker <michael.w.barker@hotmail.co.uk>
2024-03-12 11:25:18 +0000

    Add script to change innerHTML of 'games-area' element and add main functions upon click of 'play-game' button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,120 @@
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
 
-    let higherBtn = document.getElementById('higherBtn');
-    let lowerBtn = document.getElementById('lowerBtn');
+    playBtn = document.getElementById('play-game');
+    gameArea = document.getElementById('game-area');
 
-    higherBtn.addEventListener('click', () => checkGuess(true));
-    lowerBtn.addEventListener('click', () => checkGuess(false));
-})
+    playBtn.addEventListener('click', function() {
+        
+        gameArea.innerHTML =`
+        <div id="cardValue"></div>
 
-let cardValueElement = document.getElementById('cardValue');
-let currentScoreElement = document.getElementById('currentScore');
-let highScoreElement = document.getElementById('highScore');
-let drawnCardsElement = document.getElementById('drawn-cards');
+        <div id="user-area">
+
+            <div id="user-score">
+                <p>Current Score: <span id="currentScore">0</span></p>
+                <p>High Score: <span id="highScore">0</span></p>
+            </div>
+    
+            <div id="user-input">
+                <button id="higherBtn">Higher</button>
+                <button id="lowerBtn">Lower</button>
+            </div>
+        </div>
+        
+        <div id="drawn-cards"></div>
+        `;
+
+        let higherBtn = document.getElementById('higherBtn');
+        let lowerBtn = document.getElementById('lowerBtn');
+
+        higherBtn.addEventListener('click', () => checkGuess(true));
+        lowerBtn.addEventListener('click', () => checkGuess(false));
+
+        let cardValueElement = document.getElementById('cardValue');
+        let currentScoreElement = document.getElementById('currentScore');
+        let highScoreElement = document.getElementById('highScore');
+        let drawnCardsElement = document.getElementById('drawn-cards');
+
+        // Initial game state
+        let drawPile = [];
+        draw();
+        displayCard();
+        displayDrawPile();
+
+        let currentScore = 0;
+        let highScore = 0;
+
+        /**
+         * Splice a random card out of the deck
+         * Put it as the first index of the 'drawPile' array
+         */
+        function draw() {
+
+            let randomCard = Math.floor(Math.random() * deck.length);
+            let drawnCard = deck.splice(randomCard, 1)[0];
+            drawPile.unshift(drawnCard);
+        };
+
+        /**
+         * Display the first card object in the 'drawPile' array
+         */
+        function displayCard() {
+
+            cardValueElement.innerHTML = drawPile[0].pic
+        };
+
+        /**
+         * Displays the 'initials' of card objects in 'drawPile' in 'drawnCardsElement'
+         */
+        function displayDrawPile() {
+
+            for (i = 0; i < drawPile.length; i++) {
+                drawnCardsElement.innerText += drawPile[i].initials
+            };
+        };
+
+        /**
+         * Check's the user's guess 
+         */
+        function checkGuess(isHigher) {
+
+            draw();
+            let currentCard = drawPile[0];
+            let previousCard = drawPile[1];
+            let currentCardValue = currentCard.value;
+            let previousCardValue = previousCard.value;
+
+            if ((isHigher && currentCardValue > previousCardValue) || (!isHigher && currentCardValue < previousCardValue)) {
+                // Correct guess
+                currentScore++;
+                if (currentScore > highScore) {
+                    highScore = currentScore;
+                };
+            } else {
+                // Incorrect guess
+                currentScore = 0;
+                let returnCards = drawPile.splice(1, drawPile.length);
+                deck.push(...returnCards);
+                drawPile.length = 1;
+            };
+            displayCard();
+            drawnCardsElement.innerText = "";
+            displayDrawPile();
+            updateScores();
+        }
+
+        /**
+         * Udates current score and high score
+         */
+        function updateScores() {
+    
+            currentScoreElement.textContent = currentScore;
+            highScoreElement.textContent = highScore;
+        };
+
+    });
+});
 
 // Create card objects with 'name' and 'value'
 // Put into 'deck' array
@@ -20,7 +123,7 @@ let suits = ["spades", "diamonds", "clubs", "hearts"];
 let faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
 
 let firstInitials = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-let secondInitials = ["♠", "♦", "♣", "♥"]
+let secondInitials = ["♠", "♦", "♣", "♥"];
 
 
 let deck = [];
@@ -48,81 +151,4 @@ for (let suit of suits) {
 
         deck.push(card);
     }
-}
-
-// Initial game state
-let drawPile = []
-draw();
-displayCard();
-displayDrawPile();
-
-let currentScore = 0;
-let highScore = 0;
-
-/**
- * Splice a random card out of the deck
- * Put it as the first index of the 'drawPile' array
- */
-function draw() {
-
-    let randomCard = Math.floor(Math.random() * deck.length);
-    let drawnCard = deck.splice(randomCard, 1)[0];
-    drawPile.unshift(drawnCard);
-}
-
-/**
- * Display the first card object in the 'drawPile' array
- */
-function displayCard() {
-
-    cardValueElement.innerHTML = drawPile[0].pic
-}
-
-/**
- * Displays the 'initials' of card objects in 'drawPile' in 'drawnCardsElement'
- */
-function displayDrawPile() {
-
-    for (i = 0; i < drawPile.length; i++) {
-        drawnCardsElement.innerText += drawPile[i].initials
-    }
-}
-
-/**
- * Check's the user's guess 
- */
-function checkGuess(isHigher) {
-
-    draw();
-    let currentCard = drawPile[0]
-    let previousCard = drawPile[1]
-    let currentCardValue = currentCard.value;
-    let previousCardValue = previousCard.value;
-
-    if ((isHigher && currentCardValue > previousCardValue) || (!isHigher && currentCardValue < previousCardValue)) {
-        // Correct guess
-        currentScore++;
-        if (currentScore > highScore) {
-            highScore = currentScore;
-        }
-    } else {
-        // Incorrect guess
-        currentScore = 0;
-        let returnCards = drawPile.splice(1, drawPile.length);
-        deck.push(...returnCards);
-        drawPile.length = 1;
-    }
-    displayCard();
-    drawnCardsElement.innerText = ""
-    displayDrawPile();
-    updateScores();
-}
-
-/**
- * Udates current score and high score
- */
-function updateScores() {
-    
-    currentScoreElement.textContent = currentScore;
-    highScoreElement.textContent = highScore;
 }
